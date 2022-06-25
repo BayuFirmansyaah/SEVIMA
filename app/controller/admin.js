f1d63a5c3dc5451fd6fe865431d7c57e25dc7f36
bayu firmansyah <bayufirmansyah2800@gmail.com>
2022-06-25 16:33:13 +0700

    create admin mvc templating

--- app/controller/admin.js
+++ app/controller/admin.js
@@ -2,7 +2,67 @@ const controller = {};
 
 controller.index = (req, res) => {
     res.render('admin/index', {
+        title: "Admin | Little Einstein"
+    })
+}
 
+
+// account
+controller.account = (req, res) => {
+    res.render('admin/account/index', {
+        title: "Account | Little Einstein"
+    })
+}
+
+controller.register = (req, res) => {
+    res.render('admin/account/register', {
+        title: "Register | Little Einstein"
+    })
+}
+
+controller.manage_account = (req, res) => {
+    res.render('admin/account/manage', {
+        title: "Manage Account | Little Einstein"
+    })
+}
+
+
+// article
+controller.article = (req, res) => {
+    res.render('admin/article/index', {
+        title: "Article | Little Einstein"
+    })
+}
+
+controller.create_article = (req, res) => {
+    res.render('admin/article/create', {
+        title: "Create Article | Little Einstein"
+    })
+}
+
+controller.manage_article = (req, res) => {
+    res.render('admin/article/manage', {
+        title: "Manage Article | Little Einstein"
+    })
+}
+
+
+// quiz
+controller.quiz = (req, res) => {
+    res.render('admin/quiz/index', {
+        title: "Quiz | Little Einstein"
+    })
+}
+
+controller.create_quiz = (req, res) => {
+    res.render('admin/quiz/create', {
+        title: "Create Quiz | Little Einstein"
+    })
+}
+
+controller.manage_quiz = (req, res) => {
+    res.render('admin/quiz/manage', {
+        title: "Manage Quiz | Little Einstein"
     })
 }
 
--- app/routes/admin.js
+++ app/routes/admin.js
@@ -4,4 +4,20 @@ const controller = require('../controller/admin');
 
 router.get('/', controller.index);
 
+// account
+router.get('/account', controller.account);
+router.get('/register', controller.register);
+router.get('/manage/account', controller.manage_account);
+
+// article
+router.get('/article', controller.article);
+router.get('/create/article', controller.create_article);
+router.get('/manage/article', controller.manage_article)
+
+// quiz
+router.get('/quiz', controller.quiz);
+router.get('/create/quiz', controller.create_quiz);
+router.get('/manage/quiz', controller.manage_quiz);
+
+
 module.exports = router;
